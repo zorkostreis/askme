408b673c42eb62c87bfaae49a972e2a017f225ef
Eva Israelian <evais1@mail.ru>
2022-08-19 15:54:30 +0300

    delete hide from questions recources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
-  resources :questions do
-    put :hide, on: :member
-  end
+  resources :questions
 
   resource :session, only: %i[new create destroy]
   resources :users, except: %i[index]
