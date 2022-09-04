aa3960432d9142e4c04594967aea2e851c748c3b
Eva Israelian <evais1@mail.ru>
2022-09-04 08:27:31 +0300

    refactor hashtags_controller

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,11 +1,6 @@
 class HashtagsController < ApplicationController
   def show
-    @hashtag = Hashtag.find_by(name: params[:name])
-
-    if @hashtag.present?
-      @questions = @hashtag.questions.order(created_at: :desc)
-    else
-      redirect_to root_path, alert: 'Такого хэштега не существует!'
-    end
+    @hashtag = Hashtag.find_by!(name: params[:name])
+    @questions = @hashtag.questions.order(created_at: :desc)
   end
 end
